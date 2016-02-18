7952d0b2423d2bf606d7ea074a69e991eb4bf2f4
mfackrell1 <mfackrell1@mathews-mbp>
2016-02-18 11:33:42 -0700

    added class Game, InventoryItem and Scene. 1.1.16

---
+++ UltimateChess/src/byui/cit260/ultimateChess/view/StartProgramView.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ultimateChess.view;
+
+/**
+ *
+ * @author mfackrell1
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+    
+    public StartProgramView(){
+        this.promptMessage = "/nPlease enter your name:";
+        this.displayBanner();
+}
+
+    private void displayBanner() {
+        System.out.println(
+        "\n**************************************"
+       +"\n                                      "
+       +"\n This is the game of Ultimate Chess   "
+       +"\n                                      "         
+       +"\n**************************************"         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        );
+    }
+
+    public void displayStartProgramView() {
+        System.out.println("\n***displayProgramView function called ***");
+    }
+
+}
